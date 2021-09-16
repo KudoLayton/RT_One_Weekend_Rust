838683e98cb0f61f66177ee5c3f19dd45341e333
H. Lee <lhslhg@gmail.com>
2021-09-16 13:27:02 +0900

    feat: generate first image

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate image;
+
 fn main() {
-    println!("Hello, world!");
+
+    // Image
+
+    let image_width = 256;
+    let image_height = 256;
+    let mut image_buffer = Vec::<u8>::with_capacity(image_width * image_height * 3);
+
+    // Render
+
+    for j in (0..image_height).rev() {
+        for i in 0..image_width {
+            let r = (i as f64) / ((image_width - 1) as f64);
+            let g = (j as f64) / ((image_width - 1) as f64);
+            let b = 0.25f64;
+
+            let ir = (255.999 * r) as u8;
+            let ig = (255.999 * g) as u8;
+            let ib = (255.999 * b) as u8;
+
+            image_buffer.push(ir);
+            image_buffer.push(ig);
+            image_buffer.push(ib);
+        }
+    }
+    image::save_buffer(
+        "image.png", 
+        image_buffer.as_slice(), 
+        image_width as u32, 
+        image_height as u32, 
+        image::ColorType::Rgb8
+    ).expect("failed to generate image file");
 }
